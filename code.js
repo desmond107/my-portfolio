5de21c687f3ef7ad3c71a3c902c2a6387c8392e4
desmond <desmond@desmonds-MacBook-Pro.local>
2022-03-31 13:59:04 +0300

    scroll top added and linket to function and toggle class

--- code.js
+++ code.js
@@ -13,3 +13,23 @@ $(document).ready(function(){
         }else{
           $('.top').hide();
         }
+    });
+
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
